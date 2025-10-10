1416ef83f8057f48330e14ae8cc97120843d9880
khatijazaidi <khatijazaidi1304@gmail.com>
2025-10-10 23:13:50 +0500

    backend: add Forgot Password functionality

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 const logger = require('../config/logger');
+const { sendEmail } = require('../utils/sendEmail');
 
 
 const signToken = (id) =>
